349f4f675b55e3f329105503a8a1bde5ec9302a1
Arnau Abella <arnauabella@gmail.com>
2021-05-11 11:14:07 +0200

    kpi3 implemented

--- p2/src/import.py
+++ p2/src/import.py
@@ -2,21 +2,31 @@
 
 import json
 import os
+import csv
 
 from mongoclient import CustomMongoClient
 
 datasetsPath = '../datasets/'
 incomePath = datasetsPath+'income_opendata/income_opendata_neighborhood.json'
+ocupacioPath = datasetsPath + 'inhabitants_opendata/inhabitants.csv'
 lookupTablesDir = datasetsPath+'lookup_tables/'
 
 if __name__ == "__main__":
     client = CustomMongoClient()
     client.dropDB()
 
+    # Income
     with open(incomePath, 'r') as fp:
         objs = list(map(json.loads, fp.readlines()))
         client.store(objs, 'income')
 
+    # Extra dataset
+    with open(ocupacioPath, 'r', encoding='utf-8') as fp:
+        reader = csv.DictReader(fp)
+        objs = list(reader)
+        client.store(objs, 'inhabitants')
+
+    # Lookup Table
     for path in os.listdir(lookupTablesDir):
        (fileName, ext) = os.path.splitext(path)
        if ext == '.json':
@@ -24,4 +34,5 @@ if __name__ == "__main__":
                 objs = list(map(json.loads, fp.readlines()))
                 client.store(objs, fileName)
 
+    # Display what is in mongoDB
     client.statistics()
--- p2/src/main.py
+++ p2/src/main.py
@@ -11,7 +11,6 @@ from pyspark import RDD, SparkContext
 from pyspark.sql.functions import col, lit
 from pyspark.sql.types import DoubleType
 from typing import Tuple
-from pyspark import StorageLevel
 
 class SparkClient():
 
@@ -28,6 +27,7 @@ class SparkClient():
 
     # OpenDataBcn
     incomeRDD: RDD
+    inhabitantsRDD: RDD
 
     # Lookup Table
     incomeNeighborRDD: RDD
@@ -44,7 +44,7 @@ class SparkClient():
             .builder \
             .master(f"local[{num_processors}]") \
             .appName("myApp") \
-            .config('spark.driver.memory', '16g')\
+            .config('spark.driver.memory', '4g')\
             .config('spark.jars.packages', 'org.mongodb.spark:mongo-spark-connector_2.12:3.0.1') \
             .getOrCreate()
 
@@ -58,7 +58,7 @@ class SparkClient():
             print('Make sure mongoDB has the data loaded, otherwise run import.py')
             print('-'*40)
             self.loadIdealista()
-            self.loadIncome()
+            self.loadOpenData()
             self.loadLookup()
             self.__isLoaded = True
         print('Data loaded.')
@@ -80,8 +80,14 @@ class SparkClient():
                 .map(lambda x: (x['ne'], x['ne_re'])) \
                 .cache()
 
-    def loadIncome(self) -> None:
-        self.incomeRDD = self.__loadMongoRDD('income').cache()
+    def loadOpenData(self) -> None:
+        self.incomeRDD = \
+            self.__loadMongoRDD('income').cache()
+
+        self.inhabitantsRDD = \
+          self.__loadMongoRDD('inhabitants') \
+              .map(lambda x: (x['Nom_Barri'], x['Ocupacio_mitjana_(persones_ per_domicili)'])) \
+              .cache()
 
     def loadIdealista(self) -> None:
         def maxByDate(a, b) -> bool:
@@ -114,17 +120,16 @@ class SparkClient():
         return self.sparkSession\
                    .read.format("mongo") \
                    .option('uri', f"mongodb://test:1234@127.0.0.1/test.{collection}?authSource=admin") \
-                   .option('mongodb.keep_alive_ms', '50000') \
                    .load() \
                    .rdd
+                   # .option('mongodb.keep_alive_ms', '50000') \
 
     def __parseDateFromName(self, name: str) -> datetime:
         """Example: 2020_01_02_idealista"""
         str = re.compile("([\d]{4}_[\d]{2}_[\d]{2})").match(name).group(1).replace('_', '-')
         return datetime.datetime.strptime(str, '%Y-%m-%d')
 
-# Number of listings per day
-# You must average on the visualization tool
+# Average number of listings per day
 def kpi1(client: SparkClient) -> None:
     # Do not coalesce + write in a real cluster
     # since it will send all your data to the driver
@@ -139,16 +144,19 @@ def kpi1(client: SparkClient) -> None:
       .sortByKey(ascending=True) \
       .toDF(['date', 'listings_count'])\
       .write \
-      .csv('out/kp1.csv', header=True, mode='overwrite')
+      .csv('out/kpi1.csv', header=True, mode='overwrite')
 
     # Average #listings per day
     (sum, n) = rdd.map(lambda kv: (kv[1], 1)).reduce(lambda a,b: (a[0] + b[0], a[1] + b[1]))
     print(f'Average number of listings per day: {sum/n}')
+    # Average number of listings per day: 49.5
 
-# Correlation of rent price and family income per neighborhood
-# Output: district,neighbor,family,mean(price),mean(rfd)
-def kpi2(client: SparkClient) -> None:
-    def f(x: Tuple[str, Tuple[float, str]]) -> Tuple[str, float]:
+"""
+- KPI1: Correlation of rent price and family income per neighborhood
+- KPI2: Correlation of average number of inhabitants per family and family income per neighborhood
+"""
+def kpi2And3(client: SparkClient) -> None:
+    def unroll(x: Tuple[str, Tuple[float, str]]) -> Tuple[str, float]:
         (_, (price, ne_re)) = x
         return (ne_re, price)
 
@@ -157,7 +165,8 @@ def kpi2(client: SparkClient) -> None:
         return x[0]['RFD']
 
     # BOGUS: without this collect, the last stage on rdd1 loops forever.
-    #        we have been investigating this problem with no clue.
+    #
+    # The bug is probably related to how .join() works internally in pyspark with mongodb
     client.rentNeighbordRDD.collect()
 
     # RDD[(neigh: str, mean_price: float)]
@@ -166,13 +175,14 @@ def kpi2(client: SparkClient) -> None:
             .reduceByKey(lambda a,b: (a[0] + b[0], a[1] + b[1])) \
             .mapValues(lambda x: x[0]/x[1]) \
             .join(client.rentNeighbordRDD) \
-            .map(f)
+            .map(unroll)
 
     # RDD[(neigh: str, RFD: float)]
     rdd2 = client.incomeRDD \
                  .map(lambda x: (x['neigh_name'], latestRFD(x['info']))) \
                  .join(client.incomeNeighborRDD) \
-                 .map(f)
+                 .map(unroll) \
+                 .cache()
 
     # Merge data and save to local disk (do not do this in production).
     # Note: some strings are quoted (probably .csv does this)
@@ -181,17 +191,26 @@ def kpi2(client: SparkClient) -> None:
         .map(lambda kv: (kv[0], kv[1][0], kv[1][1])) \
         .toDF(['neighborhood', 'mean_price', 'rfd'])\
         .write \
-        .csv('out/kp2.csv', header=True, mode='overwrite')
+        .csv('out/kpi2.csv', header=True, mode='overwrite')
 
-# TODO
-#  * Load new dataset
-#  * New query using the new dataset
-def kpi3(client: SparkClient) -> None:
-    return None
+
+    # The bug strikes again
+    client.inhabitantsRDD.collect()
+
+    # RDD[neigh: str, inhabitants_per_listing: float]
+    rdd3 = client.inhabitantsRDD \
+                 .join(client.incomeNeighborRDD) \
+                 .map(unroll)
+
+    rdd3.join(rdd2) \
+        .coalesce(1) \
+        .map(lambda kv: (kv[0], kv[1][0], kv[1][1])) \
+        .toDF(['neighborhood', 'inhabitants_family', 'rfd'])\
+        .write \
+        .csv('out/kpi3.csv', header=True, mode='overwrite')
 
 if __name__ == "__main__":
     client  = SparkClient(num_processors=8)
     client.load()
     kpi1(client)
-    # kpi2(client)
-    # kpi3(client)
+    kpi2And3(client)
